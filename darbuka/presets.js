ca9edea0535ae87275af45130bff1c93b969b1b2
Anton Goroshkin <antihaos@gmail.com>
2026-05-02 23:23:23 +0700

    ver 3.1 darbuka

---
+++ darbuka/presets.js
@@ -0,0 +1,17 @@
+var PRESETS = {
+        'Maqsoum': 'D T D - T D T -',
+        'Baladi': 'D - T - D - T D T D',
+        'Saidi': 'D D T D T D - T',
+        'Malfuf': 'D - T - D T D T',
+        'Chiftetelli': 'D - D - T D T - D T - T',
+        'Ayoub': 'D D T D T D T D D T',
+        'Maksoum Saidi': 'D D T D T D - T D D T D T D T',
+        'Samai Thaqil': 'D D D D T D - T D T D - T D - T',
+        'Bolero': 'D D T - D D T - T D T -',
+        'Wahda Kabira': 'D - T D T D - T D D T',
+        'Wahda Sagira': 'D D T D T D T D',
+        'Fallahi': 'D T D T D D T D T D T D',
+        'Masmudi Saghir': 'D D - T - T D T',
+        'Masmudi Kabir': 'D D - T - D T D T -',
+        'Dawr Hindi': 'D - T D - T D - T - T D T - T D'
+      };
