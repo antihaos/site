9fa71817fef1980a97b0b7cc1ebb5ec7f5c975a2
Anton Goroshkin <antihaos@gmail.com>
2026-05-15 13:17:59 +0700

    ver4.0.0 darbuka

---
+++ darbuka1/presets.js
@@ -0,0 +1,15 @@
+var PRESETS = {
+        'Maqsoum': 'D-T- --T- D--- T---',
+        'Baladi': 'D-D- --T- D--- T---',
+        'Saidi': 'D-T- --D- D--- T---',
+        'Malfuf': 'D--T --T-',
+        'Ayoub': 'D--k D-T-',
+        'Chiftetelli': 'D--- T-T- --T- T--- D--- D--- T--- t-k-',
+        'Wahda': 'D--- --T- ---- T---',
+        'Wahda Kabira': 'D--- --T- ---- T--- D--- ---- T--- t-k-',
+        'Masmudi Kabira': 'D--- D--- ---- T--- D--- ---- T--- t-k-',
+        'Samai Thaqil': 'D--- t-k- t-k- T--- t-k- D--- D--- T--- t-k- t-k-',
+        'Dabka': 'T-D- --T- D--- T---',
+        'Khaleegy': 'D--D --T-',
+        'Rumba': 'D--- t-k- T-k- D---'
+      };
